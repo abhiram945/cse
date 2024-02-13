e108df9f9565032dbaaca743a9e0d5db7e561533
abhiram945 <abhiram94567@gmail.com>
2024-02-13 13:28:34 +0530

    added compiler design

---
+++ src/cd.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import './cd.css';
+
+const Cd=()=>{
+    return <div className="cd">
+        <h2>Compiler Design Lab outputs</h2>
+        <div className="exp exp3">
+            <h3>Experminet 3</h3>
+            <div className="images">
+                <img src="assets/cd/Screenshot (89).png" alt="exp3"/>
+                <img src="assets/cd/Screenshot (91).png" alt="exp3"/>
+                <img src="assets/cd/Screenshot (92).png" alt="exp3"/>
+                <img src="assets/cd/Screenshot (93).png" alt="exp3"/>
+                <img src="assets/cd/Screenshot (94).png" alt="exp3"/>
+            </div>
+        </div>
+        <div className="exp exp4">
+            <h3>Experminet 4</h3>
+            <div className="images">
+                <img src="assets/cd/Screenshot (95).png" alt="exp3"/>
+                <img src="assets/cd/Screenshot (96).png" alt="exp3"/>
+                <img src="assets/cd/Screenshot (97).png" alt="exp3"/>
+                <img src="assets/cd/Screenshot (98).png" alt="exp3"/>
+                <img src="assets/cd/Screenshot (99).png" alt="exp3"/>
+                <img src="assets/cd/Screenshot (100).png" alt="exp3"/>
+            </div>
+        </div>
+    </div>
+}
+export default Cd;
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Cn from './cn.jsx';
 import Dp from './dp.jsx';
+import Cd from './cd.jsx';
 import './index.css';
 const Home=()=>{
     return<>
@@ -11,6 +12,7 @@ const Home=()=>{
         </div>
         <nav>
             <Link to="/cse/cn">Computer Networks</Link><br/>
+            <Link to="/cse/cd">Compiler Design</Link><br/>
             <Link to="/cse/dp">One Note</Link><br/>
         </nav>
         <div className="designer">
@@ -24,6 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Routes>
             <Route exact path="/cse" element={<Home/>}></Route>
             <Route path="/cse/cn" element={<Cn/>}></Route>
+            <Route path="/cse/cd" element={<Cd/>}></Route>
             <Route path="/cse/dp" element={<Dp/>}></Route>
         </Routes>
     </BrowserRouter>
