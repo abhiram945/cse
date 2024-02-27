b65bc1f8466e53a97c0bec223ef446719819354e
abhiram945 <abhiram94567@gmail.com>
2024-02-27 10:26:36 +0530

    removed PAD

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Cn from './cn.jsx';
 import Dp from './dp.jsx';
 import Cd from './cd.jsx';
-import Pad from "./pad.jsx";
 import './index.css';
 const Home=({shrinkNav})=>{
     return<>
@@ -12,7 +11,6 @@ const Home=({shrinkNav})=>{
             <Link to="/cse">Home</Link>
             <Link to="/cse/cn">Computer Networks</Link>
             <Link to="/cse/cd">Compiler Design</Link>
-            <Link to="/cse/pad">PAD</Link>
             <Link to="/cse/dp">One Note</Link>
         </nav>
     </>
@@ -24,7 +22,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <Route exact path="/cse" element={<Home shrinkNav={false}/>}></Route>
             <Route path="/cse/cn" element={<><Home shrinkNav={true}/><Cn/></>}></Route>
             <Route path="/cse/cd" element={<><Home shrinkNav={true}/><Cd/></>}></Route>
-            <Route path="/cse/pad" element={<><Home shrinkNav={true}/><Pad/></>}></Route>
             <Route path="/cse/dp" element={<><Home shrinkNav={true}/><Dp/></>}></Route>
         </Routes>
     </BrowserRouter>
