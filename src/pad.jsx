f2ac6518364deb2036165ae1e3cc4ab454a264c7
abhiram945 <abhiram94567@gmail.com>
2024-02-27 10:22:59 +0530

    disabled exp4

--- src/pad.jsx
+++ src/pad.jsx
@@ -5,7 +5,7 @@ const Pad=()=>{
         <p className='downloadOptions'>In laptop, right click and then click on either SAVE AS or PRINT</p>
         <p className='downloadOptions'>In mobile click on OPEN</p>
         <div className="pdfsContainer">
-            <embed type="application/pdf" title="pdf" src="./assets/pad/exp4.pdf"/>
+            {/* <embed type="application/pdf" title="pdf" src="./assets/pad/exp4.pdf"/> */}
         </div>
     </div>
 }
